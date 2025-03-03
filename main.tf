ba2fcaf729356a79ba5e5a24d2bbd78c59fcfbdc
nikhil <nikhilnaidu980@gmail.com>
2025-03-03 14:21:00 -0500

    Added config to create vpc and subnets

--- main.tf
+++ main.tf
@@ -1,10 +1,31 @@
+# Provider Configuration
 provider "google" {
-  project = "your-gcp-project-id"
-  region  = "us-west2"  # Choose an appropriate region
+  region = "us-west1"
+  project = "rare-hub-452618-j9"
 }
 
 # Create the VPC
-resource "google_compute_network" "main" {
-  name                    = "main-vpc"
-  auto_create_subnetworks = false
+resource "google_compute_network" "vpc" {
+  name = "gke-vpc"
+  auto_create_subnetworks = "false" # Use custom subnet creation
+}
+
+# Create the public subnet
+resource "google_compute_subnetwork" "public_subnet" {
+  name          = "public-subnet"
+  region        = "us-west1"
+  network       = google_compute_network.vpc.id
+  ip_cidr_range = "10.0.1.0/24"
+  private_ip_google_access = false
+
+  # Enable public IPs for GKE nodes
+}
+
+# Create the private subnet (for nodes without public IPs)
+resource "google_compute_subnetwork" "private_subnet" {
+  name          = "private-subnet"
+  region        = "us-west1"
+  network       = google_compute_network.vpc.id
+  ip_cidr_range = "10.0.2.0/24"
+  private_ip_google_access = true
 }
