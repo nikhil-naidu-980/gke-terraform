d6542ca9aba4894e4465a6f29763304495d38cd6
nikhil <nikhilnaidu980@gmail.com>
2025-03-03 14:23:20 -0500

    Added inbound rule

--- main.tf
+++ main.tf
@@ -29,3 +29,17 @@ resource "google_compute_subnetwork" "private_subnet" {
   ip_cidr_range = "10.0.2.0/24"
   private_ip_google_access = true
 }
+
+# Create a firewall rule to allow external access to GKE nodes
+resource "google_compute_firewall" "gke_allow_inbound" {
+  name    = "gke-allow-inbound"
+  network = google_compute_network.vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["gke-node"]
+}
